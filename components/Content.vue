f07d081d9ed0d88ee5ad611eac996ae85c010f3c
rishan <rishan1996@hotmail.co.uk>
2021-12-30 04:39:05 +0000

    Initial Commit

---
+++ components/Content.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-dark h-full">
+        <p>test para</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+}
+</script>
---
+++ components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="NavBar" class ="bg-dark-nav flex items-center justify-between flex-wrap p-5">
+        <ul class="w-full block flex-grow flex items-center w-auto">
+            <li class="flex-grow">
+                <!-- Logo -->
+            </li>
+            <li class="flex-grow">
+                <a href="/" class="block inline-block">Home</a>
+            </li>
+            <li class="flex-grow">
+                <a href="blog" class="block inline-block">Blog</a>
+            </li>
+            <li class="flex-grow">
+                <a href="projects" class="block inline-block">Projects</a>
+            </li>
+            <li class="flex-grow">
+                <a href="aboutus" class="block inline-block">About Us</a>
+            </li>
+            <li class="flex-grow">
+                <a href="contactus" class="block inline-block">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
