b8422f74a5e3c6c805376495a2f3504b2981f0e3
rishan <rishan1996@hotmail.co.uk>
2022-03-12 17:36:09 +0000

    Minor Navbar and Footer Changes

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,25 +1,26 @@
 <template>
-    <div id="NavBar" class ="bg-dark-nav flex items-center justify-between flex-wrap p-5">
-        <ul class="w-full block flex-grow flex items-center w-auto">
-            <li class="flex-grow">
-                <!-- Logo -->
-            </li>
-            <li class="flex-grow">
-                <NuxtLink to="/">Home</NuxtLink>
-            </li>
-            <li class="flex-grow">
-                <NuxtLink to="/blog">Blog</NuxtLink>
-            </li>
-            <li class="flex-grow">
-                <NuxtLink to="/projects">Projects</NuxtLink>
-            </li>
-            <li class="flex-grow">
-                <NuxtLink to="/aboutme">About Me</NuxtLink>
-            </li>
-            <li class="flex-grow">
-                <NuxtLink to="/contactme">Contact Me</NuxtLink>
-            </li>
-        </ul>
+    <div id="NavBar" class ="container w-full bg-dark-nav flex items-center justify-between flex-wrap p-5">
+        <div id="logo" class="flex w-1/5">Logo</div>
+        <div id="links" class="flex w-4/5">
+            <div class="w-full w-1/3"></div>
+            <div class="w-full w-1/3">
+                <ul class="flex-grow flex items-center w-auto text-gray-200 hover:text-sky-400">
+                    <li class="flex-grow w-1/4">
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li class="flex-grow w-1/4">
+                        <NuxtLink to="/blog">Blog</NuxtLink>
+                    </li>
+                    <li class="flex-grow w-1/4">
+                        <NuxtLink to="/projects">Projects</NuxtLink>
+                    </li>
+                    <li class="flex-grow w-1/4">
+                        <NuxtLink to="/aboutme">About Me</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-full w-1/3"></div>
+        </div>
     </div>
 </template>
 
