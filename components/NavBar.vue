856b698cef8072ff280032b30d3dfcc99d34c75c
rishan-tk <rishan-tk@rishan.dev>
2022-12-27 21:32:17 +0000

    Updated Navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="NavBar" class ="container w-full bg-dark-nav flex items-center justify-between flex-wrap p-5">
-        <div id="logo" class="flex w-1/5"><img src="~/assets/img/logo-ts.png"/></div>
-        <div id="links" class="flex w-4/5">
+    <div id="NavBar" class ="container w-full bg-dark-nav flex items-center justify-between flex-wrap">
+        <div id="logo" class="flex"><img src="~/assets/img/logo-ts.png"/></div>
+        <div id="links" class="flex">
             <div class="w-full w-1/3"></div>
             <div class="w-full w-1/3">
                 <ul class="flex-grow flex items-center w-auto text-gray-200 hover:text-sky-400">
