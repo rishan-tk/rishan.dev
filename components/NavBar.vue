01ee919b2f407f76a30e521a90114418b78d63a1
rishan-tk <rishan-tk@rishan.dev>
2023-01-19 20:20:18 +0000

    Minor navbar updates

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="NavBar" class ="container w-full bg-dark-nav flex items-center justify-between flex-wrap">
         <div id="logo" class="flex"><img src="~/assets/img/logo-ts.png"/></div>
-        <div id="links" class="flex">
-            <div class="w-full w-1/3"></div>
-            <div class="w-full w-1/3">
-                <ul class="flex-grow flex items-center w-auto text-gray-200 hover:text-sky-400">
+        <div id="linksContainer" class="flex">
+            <div class="linksPadding"></div>
+            <div id="navLinks">
+                <ul id="navlinks" class="flex-grow flex text-gray-200 hover:text-sky-400">
                     <li class="flex-grow w-1/4">
                         <NuxtLink to="/">Home</NuxtLink>
                     </li>
@@ -19,7 +19,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="w-full w-1/3"></div>
+            <div class="linksPadding"></div>
         </div>
     </div>
 </template>
