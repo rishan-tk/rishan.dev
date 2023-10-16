da827914a99331d49a0e8460bd6097d62eac9360
rishan-tk <rishan-tk@rishan.dev>
2023-10-17 00:11:16 +0100

    Updated skills page and styling

--- components/Contents/S/Content.vue
+++ components/Contents/S/Content.vue
@@ -2,22 +2,42 @@
     <div class="heading container">SKILLS</div>
     <div class="main-content container">
 
-        <p>Skills I have aquired</p>
+        <div class="skills">
+            <p class="caption">Skills I have acquired</p>
+            <ul>
+                <li>
+                    <NuxtImg src="/img/skills/cpp-logo.png" alt="C Plus Plus Logo"/>
+                    <p>C++</p>
+                </li>
+                <li>
+                    <NuxtImg src="/img/skills/python-logo.png" alt="Python Logo"/>
+                    <p>Python</p>
+                </li>
+                <li>
+                    <NuxtImg src="/img/skills/html5-logo.png" alt="HTML 5 Logo"/>
+                    <p>HTML5</p>
+                </li>
+            </ul>
+        </div>
 
-        <ul>
-            <li>C++</li>
-            <li>Python</li>
-            <li>HTML5</li>
-        </ul>
+        <div class="tools">
+            <p class="caption">Tools used</p>
 
-        <p>Tools used</p>
-
-        <ul>
-            <li>Visual Studio Code</li>
-            <li>Visual Studio</li>
-            <li>Github</li>
-        </ul>
-
+            <ul class="flex px-5">
+                <li class="px-5">
+                    <NuxtImg src="/img/skills/vscode-logo.png" alt="Visual Studio Code Logo"/>
+                    <p>Visual Studio Code</p>
+                </li>             
+                <li class="px-5">
+                    <NuxtImg src="/img/skills/vs-logo.png" alt="Visual Studio Logo"/>
+                    <p>Visual Studio</p>
+                </li>
+                <li class="px-5">
+                    <NuxtImg src="/img/skills/github-logo.png" alt="Github Logo"/>
+                    <p>Github</p>
+                </li>
+            </ul>
+        </div>
 
     </div>
 </template>
