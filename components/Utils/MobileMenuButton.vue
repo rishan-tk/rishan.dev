72a9e995bb428c789edd5b656ddec7f9d830f72c
rishan-tk <rishan-tk@rishan.dev>
2023-10-19 00:39:55 +0100

    Fixed mobile nav bug

--- components/Utils/MobileMenuButton.vue
+++ components/Utils/MobileMenuButton.vue
@@ -15,15 +15,36 @@
                 const navbar = document.getElementById('navbar');
                 const computedStyle = window.getComputedStyle(navbar);
 
-                console.log(computedStyle)
-
                 // Change the display property based on the condition
                 if (computedStyle.getPropertyValue('display') === 'flex') {
                     navbar.style.display = 'none'; 
                 } else if (computedStyle.getPropertyValue('display') === 'none'){
                     navbar.style.display = 'flex'; 
                 }
-            }
+            },
+            handleResize() {
+                const windowWidth = window.innerWidth;
+                const navbar = document.getElementById('navbar');
+                const computedStyle = window.getComputedStyle(navbar);
+
+                // Check if the window width is greater than a certain value (for example, 768 pixels)
+                if (windowWidth > 768) {
+                    if (computedStyle.getPropertyValue('display') === 'none'){
+                        navbar.style.display = 'flex';
+                    }
+                    
+                }
+            },
+        },
+
+        mounted() {
+            // Add an event listener for window resize event
+            window.addEventListener('resize', this.handleResize);
+        },
+
+        beforeDestroy() {
+            // Remove the event listener when the component is destroyed
+            window.removeEventListener('resize', this.handleResize);
         }
     });
 </script>
